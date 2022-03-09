9a22f78231f4f9ba0d46a3379b9cb693abf515c9
daneshvar-amrollahi <amrollahi.daneshvar@gmail.com>
2022-03-09 21:10:46 +0100

    Added basic files for client

---
+++ client/include/Client.hpp
@@ -0,0 +1,11 @@
+#ifndef __CLIENT_H__
+#define __CLIENT_H__
+
+class Client
+{
+public:
+    Client();
+    void run();
+};
+
+#endif
---
+++ client/src/Client.cpp
@@ -0,0 +1,9 @@
+#include "Client.hpp"
+
+Client::Client() {
+    //I guess this will always be empty
+}
+
+void Client::run() {
+    
+}
