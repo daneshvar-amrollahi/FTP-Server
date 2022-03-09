a54f5b3478ee2136f991b1f65465906caf979132
daneshvar-amrollahi <amrollahi.daneshvar@gmail.com>
2022-03-09 21:10:21 +0100

    Added server run method

--- server/src/Server.cpp
+++ server/src/Server.cpp
@@ -3,3 +3,7 @@
 Server::Server() {
     //I guess this will always be empty
 }
+
+void Server::run() {
+    
+}
--- server/src/main.cpp
+++ server/src/main.cpp
@@ -4,5 +4,7 @@ using namespace std;
 
 int main(int argc, char const *argv[]) {
     Server server = Server();
+    //config..
+    server.run();
     return 0;
 }
